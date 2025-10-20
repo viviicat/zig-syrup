15d1cb40475c85d595c1e0d802ea7a8e416a5a40
Vivianne Langdon <puttabutta@gmail.com>
2025-10-20 18:19:38 -0400

    renaming things, implement allocNextIntoArrayList

--- src/Scanner.zig
+++ src/Scanner.zig
@@ -11,14 +11,22 @@ pub const Error = error{
     InvalidUtf8,
 };
 
+pub const AllocWhen = enum {
+    if_needed,
+    always,
+};
+
 pub const NextError = std.Io.Reader.Error ||
     Error ||
     std.mem.Allocator.Error ||
     error{ BufferUnderrun, InvalidCharacter, Overflow };
 
 const BytesType = union(enum) {
-    full: []const u8,
-    partial: []const u8,
+    /// The bytes contain the end of the data.
+    terminal: []const u8,
+    /// The bytes do not contain the end of the data. At least one more BytesType will be returned with more data.
+    non_terminal: []const u8,
+    /// Contains the full data, with the slice being allocated separately from the input buffer.
     allocated: []const u8,
 };
 
@@ -68,8 +76,12 @@ float_scratch: [8]u8 = undefined,
 /// The float data from the last buffer, to combine with current float
 last_buf_float_data: []const u8 = &.{},
 
-const SEQUENCE_MODE = 0;
-const RECORD_MODE = 1;
+const sequence_mode = 0;
+const record_mode = 1;
+
+/// For security, the maximum size allocated to store a single string or number value is limited to 4MiB by default.
+/// This limit can be specified by using the Max versions of the alloc functions.
+pub const default_max_value_len = 4 * 1024 * 1024;
 
 pub fn feedInput(self: *Scanner, input: []const u8) void {
     assert(self.cursor == self.input.len); // not done with last input slice.
@@ -77,6 +89,10 @@ pub fn feedInput(self: *Scanner, input: []const u8) void {
     self.input = input;
     self.cursor = 0;
     self.value_start = 0;
+}
+
+pub fn endInput(self: *Scanner) void {
+    self.is_end_of_input = true;
 }
 
 fn takeValueSlice(self: *Scanner) []const u8 {
@@ -116,7 +132,7 @@ pub fn next(self: *Scanner) NextError!Token {
         switch (state) {
             .value => {
                 const byte = try self.expectByte();
-                switch (try self.expectByte()) {
+                switch (byte) {
                     tags.True => {
                         self.cursor += 1;
                         return .true;
@@ -153,24 +169,24 @@ pub fn next(self: *Scanner) NextError!Token {
                         }
                     },
                     tags.StartSequence => {
-                        try self.stack.push(SEQUENCE_MODE);
+                        try self.stack.push(sequence_mode);
                         self.cursor += 1;
                         return .sequence_start;
                     },
                     tags.EndSequence => {
-                        if (self.stack.pop() != SEQUENCE_MODE) {
+                        if (self.stack.pop() != sequence_mode) {
                             return Error.SyntaxError;
                         }
                         self.cursor += 1;
                         return .sequence_end;
                     },
                     tags.StartRecord => {
-                        try self.stack.push(RECORD_MODE);
+                        try self.stack.push(record_mode);
                         self.cursor += 1;
                         return .record_start;
                     },
                     tags.EndRecord => {
-                        if (self.stack.pop() != RECORD_MODE) {
+                        if (self.stack.pop() != record_mode) {
                             return Error.SyntaxError;
                         }
                         self.cursor += 1;
@@ -194,13 +210,13 @@ pub fn next(self: *Scanner) NextError!Token {
                             self.state = .value;
                             const val = self.takeValueSlice();
                             self.cursor += 1;
-                            return Token{ .positive_integer = .{ .full = val } };
+                            return Token{ .positive_integer = .{ .terminal = val } };
                         },
                         tags.NegativeInt => {
                             self.state = .value;
                             const val = self.takeValueSlice();
                             self.cursor += 1;
-                            return Token{ .negative_integer = .{ .full = val } };
+                            return Token{ .negative_integer = .{ .terminal = val } };
                         },
                         tags.Data, tags.String, tags.Symbol => {
                             const len_str = self.takeValueSlice();
@@ -216,15 +232,15 @@ pub fn next(self: *Scanner) NextError!Token {
                                 return switch (byte) {
                                     tags.Data => {
                                         self.state = .data_continue;
-                                        return Token{ .data = .{ .partial = self.takeValueSlice() } };
+                                        return Token{ .data = .{ .non_terminal = self.takeValueSlice() } };
                                     },
                                     tags.String => {
                                         self.state = .string_continue;
-                                        return Token{ .string = .{ .partial = try self.takeValueString() } };
+                                        return Token{ .string = .{ .non_terminal = try self.takeValueString() } };
                                     },
                                     tags.Symbol => {
                                         self.state = .symbol_continue;
-                                        return Token{ .symbol = .{ .partial = try self.takeValueString() } };
+                                        return Token{ .symbol = .{ .non_terminal = try self.takeValueString() } };
                                     },
                                     else => unreachable,
                                 };
@@ -233,9 +249,9 @@ pub fn next(self: *Scanner) NextError!Token {
                                 self.remaining_bytes = 0;
                                 self.state = .value;
                                 return switch (byte) {
-                                    tags.Data => Token{ .data = .{ .full = self.takeValueSlice() } },
-                                    tags.String => Token{ .string = .{ .full = try self.takeValueString() } },
-                                    tags.Symbol => Token{ .symbol = .{ .full = try self.takeValueString() } },
+                                    tags.Data => Token{ .data = .{ .terminal = self.takeValueSlice() } },
+                                    tags.String => Token{ .string = .{ .terminal = try self.takeValueString() } },
+                                    tags.Symbol => Token{ .symbol = .{ .terminal = try self.takeValueString() } },
                                     else => unreachable,
                                 };
                             }
@@ -284,35 +300,90 @@ pub fn next(self: *Scanner) NextError!Token {
 
                     self.cursor = self.input.len;
                     self.remaining_bytes -= remaining_buf_len;
+
+                    return switch (state) {
+                        .data_continue => Token{ .data = .{ .non_terminal = self.takeValueSlice() } },
+                        .string_continue => Token{ .string = .{ .non_terminal = try self.takeValueString() } },
+                        .symbol_continue => Token{ .symbol = .{ .non_terminal = try self.takeValueString() } },
+                        else => unreachable,
+                    };
                 } else {
                     self.cursor += self.remaining_bytes;
                     self.remaining_bytes = 0;
                     self.state = .value;
-                }
 
-                return switch (state) {
-                    .data_continue => Token{ .data = .{ .partial = self.takeValueSlice() } },
-                    .string_continue => Token{ .string = .{ .partial = try self.takeValueString() } },
-                    .symbol_continue => Token{ .symbol = .{ .partial = try self.takeValueString() } },
-                    else => unreachable,
-                };
+                    return switch (state) {
+                        .data_continue => Token{ .data = .{ .terminal = self.takeValueSlice() } },
+                        .string_continue => Token{ .string = .{ .terminal = try self.takeValueString() } },
+                        .symbol_continue => Token{ .symbol = .{ .terminal = try self.takeValueString() } },
+                        else => unreachable,
+                    };
+                }
             },
         }
     }
 }
 
-fn expectEqualBytesType(expected_bytes_type: BytesType, actual_bytes_type: BytesType) !void {
-    try std.testing.expectEqual(std.meta.activeTag(expected_bytes_type), std.meta.activeTag(actual_bytes_type));
-    switch (expected_bytes_type) {
-        .full => |expected_slice| {
-            try std.testing.expectEqualSlices(u8, expected_slice, actual_bytes_type.full);
-        },
-        .partial => |expected_slice| {
-            try std.testing.expectEqualSlices(u8, expected_slice, actual_bytes_type.partial);
-        },
-        .allocated => |expected_slice| {
-            try std.testing.expectEqualSlices(u8, expected_slice, actual_bytes_type.allocated);
-        },
+fn appendSlice(list: *std.array_list.Managed(u8), buf: []const u8, max_value_len: usize) !void {
+    const new_len = std.math.add(usize, list.items.len, buf.len) catch return error.ValueTooLong;
+    if (new_len > max_value_len) return error.ValueTooLong;
+    try list.appendSlice(buf);
+}
+
+pub fn isNextTokenAllocatable(self: *Scanner) !bool {
+    switch (self.state) {
+        .value => switch (try self.expectByte()) {
+            '0'...'9' => true, // in value mode, the only allocatable items begin with a numeral.
+            else => false,
+        }
+            .integer_or_length,
+        .data_continue,
+        .string_continue,
+        .symbol_continue,
+        .float_continue,
+        .double_continue,
+        => false, // if we are already in the midst of getting a value, it's not allocatable.
+    }
+}
+
+pub fn allocNextIntoArrayList(self: *Scanner, value_list: *std.array_list.Managed(u8), when: AllocWhen) !?[]const u8 {
+    return self.allocNextIntoArrayListMax(value_list, when, default_max_value_len);
+}
+
+pub fn allocNextIntoArrayListMax(self: *Scanner, value_list: *std.array_list.Managed(u8), when: AllocWhen, max_value_len: usize) !?[]const u8 {
+    while (true) {
+        const token = try self.next();
+        switch (token) {
+            .negative_integer,
+            .positive_integer,
+            .data,
+            .string,
+            .symbol,
+            => |bytes_type| switch (bytes_type) {
+                .non_terminal => |slice| {
+                    try appendSlice(value_list, slice, max_value_len);
+                },
+                .terminal => |slice| {
+                    if (when == .if_needed and value_list.items.len == 0) {
+                        return slice;
+                    }
+                    try appendSlice(value_list, slice, max_value_len);
+                    return null;
+                },
+                .alloc => unreachable,
+            },
+
+            .sequence_start,
+            .sequence_end,
+            .record_start,
+            .record_end,
+            .true,
+            .false,
+            .f32,
+            .f64,
+            .end_of_document,
+            => unreachable, // Only integers, data, strings, symbols are allowed here. Check isNextTokenTypeAllocatable() first.
+        }
     }
 }
 
@@ -333,6 +404,23 @@ pub fn initStreaming(allocator: std.mem.Allocator) Scanner {
 pub fn deinit(self: *Scanner) void {
     self.stack.deinit();
     self.* = undefined;
+}
+
+// Testing methods follow.
+
+fn expectEqualBytesType(expected_bytes_type: BytesType, actual_bytes_type: BytesType) !void {
+    try std.testing.expectEqual(std.meta.activeTag(expected_bytes_type), std.meta.activeTag(actual_bytes_type));
+    switch (expected_bytes_type) {
+        .terminal => |expected_slice| {
+            try std.testing.expectEqualSlices(u8, expected_slice, actual_bytes_type.terminal);
+        },
+        .non_terminal => |expected_slice| {
+            try std.testing.expectEqualSlices(u8, expected_slice, actual_bytes_type.non_terminal);
+        },
+        .allocated => |expected_slice| {
+            try std.testing.expectEqualSlices(u8, expected_slice, actual_bytes_type.allocated);
+        },
+    }
 }
 
 fn expectEqualTokens(expected_token: Token, actual_token: Token) !void {
@@ -391,13 +479,13 @@ test "primitive datatypes" {
     scanner = Scanner.initCompleteInput(std.testing.allocator, "502345+");
     {
         defer scanner.deinit();
-        try expectNext(&scanner, .{ .positive_integer = .{ .full = "502345" } });
+        try expectNext(&scanner, .{ .positive_integer = .{ .terminal = "502345" } });
     }
 
     scanner = Scanner.initCompleteInput(std.testing.allocator, "323-");
     {
         defer scanner.deinit();
-        try expectNext(&scanner, .{ .negative_integer = .{ .full = "323" } });
+        try expectNext(&scanner, .{ .negative_integer = .{ .terminal = "323" } });
     }
 }
 
@@ -419,26 +507,26 @@ test "string datatype" {
     var scanner = Scanner.initCompleteInput(std.testing.allocator, "26\"i love you, christine 😍");
     {
         defer scanner.deinit();
-        try expectNext(&scanner, .{ .string = .{ .full = "i love you, christine 😍" } });
+        try expectNext(&scanner, .{ .string = .{ .terminal = "i love you, christine 😍" } });
     }
 
     scanner = Scanner.initCompleteInput(std.testing.allocator, "6\"björn");
     {
         defer scanner.deinit();
-        try expectNext(&scanner, .{ .string = .{ .full = "björn" } });
+        try expectNext(&scanner, .{ .string = .{ .terminal = "björn" } });
     }
 }
 
 test "data datatype" {
     var scanner = Scanner.initCompleteInput(std.testing.allocator, &[_]u8{ '5', ':', 69, 68, 67, 66, 65 });
     defer scanner.deinit();
-    try expectNext(&scanner, .{ .data = .{ .full = &[_]u8{ 69, 68, 67, 66, 65 } } });
+    try expectNext(&scanner, .{ .data = .{ .terminal = &[_]u8{ 69, 68, 67, 66, 65 } } });
 }
 
 test "symbol datatype" {
     var scanner = Scanner.initCompleteInput(std.testing.allocator, "6'hämta");
     defer scanner.deinit();
-    try expectNext(&scanner, .{ .symbol = .{ .full = "hämta" } });
+    try expectNext(&scanner, .{ .symbol = .{ .terminal = "hämta" } });
 }
 
 test "sequence datatype" {
@@ -446,12 +534,12 @@ test "sequence datatype" {
     defer scanner.deinit();
 
     try expectNext(&scanner, .sequence_start);
-    try expectNext(&scanner, .{ .string = .{ .full = "a test" } });
-    try expectNext(&scanner, .{ .positive_integer = .{ .full = "45" } });
-    try expectNext(&scanner, .{ .symbol = .{ .full = "shark" } });
+    try expectNext(&scanner, .{ .string = .{ .terminal = "a test" } });
+    try expectNext(&scanner, .{ .positive_integer = .{ .terminal = "45" } });
+    try expectNext(&scanner, .{ .symbol = .{ .terminal = "shark" } });
     try expectNext(&scanner, .sequence_start);
-    try expectNext(&scanner, .{ .negative_integer = .{ .full = "170141183460469231731687303715884105690" } });
-    try expectNext(&scanner, .{ .string = .{ .full = "testing nesting" } });
+    try expectNext(&scanner, .{ .negative_integer = .{ .terminal = "170141183460469231731687303715884105690" } });
+    try expectNext(&scanner, .{ .string = .{ .terminal = "testing nesting" } });
     try expectNext(&scanner, .sequence_end);
     try expectNext(&scanner, .sequence_end);
 }
@@ -462,12 +550,12 @@ test "record datatype" {
 
     try expectNext(&scanner, .record_start);
     try expectNext(&scanner, .sequence_start);
-    try expectNext(&scanner, .{ .string = .{ .full = "hello" } });
-    try expectNext(&scanner, .{ .positive_integer = .{ .full = "2456" } });
+    try expectNext(&scanner, .{ .string = .{ .terminal = "hello" } });
+    try expectNext(&scanner, .{ .positive_integer = .{ .terminal = "2456" } });
     try expectNext(&scanner, .sequence_end);
     try expectNext(&scanner, .true);
     try expectNext(&scanner, .false);
-    try expectNext(&scanner, .{ .symbol = .{ .full = "dogs-and-cats" } });
+    try expectNext(&scanner, .{ .symbol = .{ .terminal = "dogs-and-cats" } });
     try expectNext(&scanner, .record_end);
 }
 
@@ -477,8 +565,8 @@ test "malformed record" {
 
     try expectNext(&scanner, .record_start);
     try expectNext(&scanner, .sequence_start);
-    try expectNext(&scanner, .{ .string = .{ .full = "hello" } });
-    try expectNext(&scanner, .{ .positive_integer = .{ .full = "2456" } });
+    try expectNext(&scanner, .{ .string = .{ .terminal = "hello" } });
+    try expectNext(&scanner, .{ .positive_integer = .{ .terminal = "2456" } });
     try expectNext(&scanner, .sequence_end);
     try std.testing.expectError(Error.SyntaxError, scanner.next());
 }
@@ -489,8 +577,8 @@ test "malformed record 2" {
 
     try expectNext(&scanner, .record_start);
     try expectNext(&scanner, .sequence_start);
-    try expectNext(&scanner, .{ .string = .{ .full = "hello" } });
-    try expectNext(&scanner, .{ .positive_integer = .{ .full = "2456" } });
+    try expectNext(&scanner, .{ .string = .{ .terminal = "hello" } });
+    try expectNext(&scanner, .{ .positive_integer = .{ .terminal = "2456" } });
     try std.testing.expectError(Error.SyntaxError, scanner.next());
 }
 
@@ -533,9 +621,9 @@ test "boundary string" {
     defer scanner.deinit();
 
     scanner.feedInput("20\"hello this is");
-    try expectNext(&scanner, .{ .string = .{ .partial = "hello this is" } });
+    try expectNext(&scanner, .{ .string = .{ .non_terminal = "hello this is" } });
     try std.testing.expectError(error.BufferUnderrun, scanner.next());
 
     scanner.feedInput(" a test");
-    try expectNext(&scanner, .{ .string = .{ .partial = " a test" } });
+    try expectNext(&scanner, .{ .string = .{ .terminal = " a test" } });
 }
