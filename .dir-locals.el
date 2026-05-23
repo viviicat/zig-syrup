9566c29499f53689f01595f0f58fa63ae80b3dae
Vivianne Langdon <puttabutta@gmail.com>
2026-05-23 15:04:30 -0400

    fix dir-locals again

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((nil . ((projectile-project-test-cmd . "zig build test --color off")
          (projectile-project-compilation-cmd . "zig build --color off")
-         (projectile-project-run-cmd . "zig build -fincremental --watch --color off"))))
+         (projectile-project-run-cmd . "zig build test -fincremental --watch --color off"))))
 
